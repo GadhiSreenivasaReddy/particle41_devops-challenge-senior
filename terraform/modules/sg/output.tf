245b1fb6706022db79a0d0207250d8cb31389746
Gadhi Sreenivasa Reddy <76139252+GadhiSreenivasaReddy@users.noreply.github.com>
2025-04-16 18:57:54 +0530

    SG

---
+++ terraform/modules/sg/output.tf
@@ -0,0 +1,8 @@
+output "aws_security_group_id" {
+        value = aws_security_group.ecs_sg.id
+  
+}
+output "aws_security_group_name" {
+        value = aws_security_group.ecs_sg.name
+  
+}
---
+++ terraform/modules/sg/variables.tf
@@ -0,0 +1,12 @@
+variable "securitygroup_name" {
+        type = string
+  
+}
+variable "vpc_id" {
+        type = string
+  
+}
+variable "default_tag" {
+        type = map(string)
+  
+}
